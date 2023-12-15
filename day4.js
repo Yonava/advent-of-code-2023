b45f36748003b9a8991ce4fc975914d6f68ce5bb
yonava <yona@vossandreae.com>
2023-12-15 11:00:02 -0500

    advent of code day 4 pt 2

--- day4.js
+++ day4.js
@@ -1,12 +1,38 @@
 const { dayFour } = require('./inputs')
 
 const input = dayFour.split('\n').map((line) => line.split(':')[1].split('|')).map(([ticket, winners]) => {
-  return [ticket.split(' ').filter(i => i), winners.split(' ').filter(i => i)]
+  return [ticket.split(' ').filter(i => i), new Set(winners.split(' ').filter(i => i))]
 });
 
-const res = input.reduce((acc, [ticket, winners]) => {
-  const matches = ticket.reduce((acc, num) => winners.includes(num) ? acc + 1 : acc, 0)
-  return !matches ? acc : acc + 2**(matches - 1)
+// part 1
+// const out = input.reduce((acc, [ticket, winners]) => {
+//   const matches = ticket.reduce((acc, num) => winners.has(num) ? acc + 1 : acc, 0)
+//   return !matches ? acc : acc + 2**(matches - 1)
+// }, 0)
+
+// part 2
+const matches = input.reduce((acc, [ticket, winners], index) => {
+  return acc.set(index + 1, ticket.reduce((acc, num) => winners.has(num) ? acc + 1 : acc, 0))
+}, new Map())
+
+const memo = new Map()
+const dfs = (id) => {
+  if (memo.get(id) !== undefined) return memo.get(id)
+
+  const matchNum = matches.get(id)
+  if (matchNum === 0) return 1
+
+  let sum = 1
+  for (let i = 1; i <= matchNum; i++) sum += dfs(id + i)
+  memo.set(id, sum)
+  return sum
+}
+
+const out = input.reduce((acc, _, index) => {
+  const id = index + 1
+  dfs(id)
+  const nMatches = matches.get(id)
+  return acc + (nMatches ? memo.get(id) : 1)
 }, 0)
 
-console.log(res)
+console.log(out)
