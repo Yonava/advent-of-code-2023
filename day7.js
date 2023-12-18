d619507c153507540e42fd92adacbd2f982fbf0f
yonava <yona@vossandreae.com>
2023-12-18 10:19:05 -0700

    aoc day 7 final

--- day7.js
+++ day7.js
@@ -1,29 +1,28 @@
 const { daySeven, daySevenSm } = require('./inputs')
+const PART_2 = true
 
-const hands = daySeven.split('\n').map(line => [line.split(' ')[0], Number(line.split(' ')[1])])
+const hands = daySevenSm.split('\n').map(line => [line.split(' ')[0], Number(line.split(' ')[1])])
 
-const rankings = [
-  (cards) => new Set(cards).size === 1,
-  (cards) => cards.some(card => cards.filter(_card => _card === card).length === 4),
-  (cards) => new Set(cards).size === 2,
-  (cards) => cards.some(card => cards.filter(_card => _card === card).length === 3),
-  (cards) => new Set(cards).size === 3,
-  (cards) => new Set(cards).size === 4,
-  () => true
-]
+const frq = (card, cards) => cards.split('').filter(_card => _card === card).length
+const handify = (h) => h.split('').sort((a, b) => frq(b, h) !== frq(a, h) ? frq(a, h) - frq(b, h) : a > b ? -1 : 1)
 
-const cardRank = 'AKQJT98765432'
+const rankings = new Array(4).fill(0).map((_, i) => (cards) => cards.slice(i).every(card => cards.at(-1) === card))
+rankings.splice(2, 0, (cards) => new Set(cards).size === 2)
+rankings.splice(4, 0, (cards) => new Set(cards).size === 3)
+rankings.push(() => true)
+
+const getRank = (rank, hand) => rank(PART_2 ? hand.filter(c => c !== 'J') : hand)
+const cardRank = PART_2 ? 'AKQT98765432J' : 'AKQJT98765432'
 
 const out = hands
-  .map(([hand, bet]) => ({ rank: rankings.findIndex((rank) => rank(hand.split(''))), bet, hand }))
-  .sort((handA, handB) => {
-    if (handA.rank !== handB.rank) return handA.rank - handB.rank
-    for (let i = 0; i < handA.hand.length; i++) {
-      if (handA.hand[i] === handB.hand[i]) continue
-      return cardRank.indexOf(handA.hand[i]) - cardRank.indexOf(handB.hand[i])
+  .map(([hand, bet]) => ({ rank: rankings.findIndex((rank) => getRank(rank, handify(hand))), bet, hand }))
+  .sort((a, b) => {
+    if (a.rank !== b.rank) return b.rank - a.rank
+    for (let i = 0; i < a.hand.length; i++) {
+      if (a.hand[i] === b.hand[i]) continue
+      return cardRank.indexOf(b.hand[i]) - cardRank.indexOf(a.hand[i])
     }
   })
-  .reverse()
   .reduce((acc, { bet }, index) => acc + (index + 1) * bet, 0)
 
 console.log(out)
