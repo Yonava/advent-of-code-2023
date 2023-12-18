55ffdd29a013188cf59b73dc5f6ea90f6e634a4c
yonava <yona@vossandreae.com>
2023-12-18 12:10:36 -0700

    day 8 pt 1

---
+++ day8.js
@@ -0,0 +1,21 @@
+const { dayEight, dayEightSm } = require('./inputs')
+
+const seq = dayEight.split('\n').shift().split('')
+const rawMap = dayEight.split('\n').slice(2)
+
+const pMap = rawMap.reduce((acc, curr) => {
+  const [node, lr] = curr.split(' = ')
+  return acc.set(node, lr.slice(1, lr.length - 1).split(', '))
+}, new Map())
+
+const map = Array.from(pMap).map(i => i[0]).reduce((m, loc) => m.set(loc, seq.reduce((curr, mv) => {
+  return pMap.get(curr)[mv == 'L' ? 0 : 1]
+}, loc)), new Map())
+
+let currentLocation = 'AAA'
+while (currentLocation !== 'ZZZ') {
+  currentLocation = map.get(currentLocation)
+  out += seq.length
+}
+
+console.log(out)
