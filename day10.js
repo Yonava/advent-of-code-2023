61863ac0e7ca7f0f32336e2ee7841e1b3e6bd46a
yonava <yona@vossandreae.com>
2023-12-20 17:03:14 -0700

    god damn, day 10 is something

---
+++ day10.js
@@ -0,0 +1,49 @@
+const { dayTen, dayTenSm } = require('./inputs')
+
+const dirs = {
+  n: [-1, 0],
+  e: [0, 1],
+  s: [1, 0],
+  w: [0, -1]
+}
+
+const conns = {
+  '|': [dirs.n, dirs.s],
+  '-': [dirs.e, dirs.w],
+  'L': [dirs.n, dirs.e],
+  'J': [dirs.n, dirs.w],
+  '7': [dirs.s, dirs.w],
+  'F': [dirs.s, dirs.e]
+}
+
+// parse pipe system
+const pipes = dayTen.split('\n').map(l => l.split(''))
+
+let startCoords;
+for (let row = 0; row < pipes.length; row++) {
+  for (let col = 0; col < pipes[0].length; col++) {
+    if (pipes[row][col] !== 'S') continue
+    startCoords = [row, col]
+    break
+  }
+  if (startCoords) break
+}
+
+const rcStr = (r, c) => `${r},${c}`
+
+let curr = [21, 30];
+let prev = rcStr(21, 29);
+let s = 0
+
+while (pipes[curr[0]][curr[1]] !== 'S') {
+  const [row, col] = curr
+  const currentPipe = pipes[row][col]
+  const pipeConns = conns[currentPipe]
+  const adjustment = pipeConns.find(([r, c]) => rcStr(r + row, c + col) !== prev)
+  console.log('go to:', adjustment.map((adj, i) => adj + curr[i]))
+  prev = rcStr(...curr)
+  curr = adjustment.map((adj, i) => adj + curr[i])
+  s++
+}
+
+console.log(Math.floor(s / 2) + 1)
