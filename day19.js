886fb62b86419aaf802468c7be3bf4eb22a7d293
yonava <yona@vossandreae.com>
2023-12-23 11:33:00 -0700

    day nineteen cleanup

--- day19.js
+++ day19.js
@@ -3,59 +3,37 @@ const { dayNineteen, dayNineteenSm } = require('./inputs')
 const parseWorkflow = (str) => {
   const [name, logic] = str.split('{')
   const ins = logic.slice(0, -1).split(',')
-  const res = ins.pop()
-  const process = ins.map(i => {
-    const [des, sendTo] = i.split(':')
-    return {
-      logic: { prop: des[0], op: des[1], num: parseInt(des.slice(2)) },
-      sendTo
-    }
-  })
-
-  const fn = (obj) => {
-    for (const { logic, sendTo } of process) {
-      const { prop, op, num } = logic
-      switch (op) {
-        case '>':
-          if (obj[prop] > num) return sendTo
-          break
-        case '<':
-          if (obj[prop] < num) return sendTo
-          break
-        default:
-          throw new Error('Operation Not Accounted For!')
-      }
-    }
-    return res
-  }
+  const otherwise = ins.pop()
+  const conditions = ins.map(i => i.split(':'))
 
   return {
     name,
-    fn,
+    fn: (obj) => {
+      for (const [cond, sendTo] of conditions) {
+        if (eval(`obj.${cond}`)) return sendTo
+      }
+      return otherwise
+    },
   }
 }
 
-const parseObj = (str) => {
-  return str.slice(1, -1).split(',').reduce((acc, curr) => {
-    acc[curr[0]] = parseInt(curr.slice(2))
-    return acc
-  }, {})
-}
+const parseObj = (str) => str.slice(1, -1).split(',').reduce((acc, curr) => {
+  acc[curr[0]] = parseInt(curr.slice(2))
+  return acc
+}, {})
 
 const [workflows, objs] = dayNineteen.split('\n\n').map((inp, i) => inp.split('\n').map((str) => {
   return i ? parseObj(str) : parseWorkflow(str)
 }))
 
-const intakeWorkflow = workflows.find(({ name }) => name === 'in')
-
-const figureOut = (obj, wf) => {
+const figureOut = (obj, wf = workflows.find(({ name }) => name === 'in')) => {
   if (wf.fn(obj) === 'A') return true
   else if (wf.fn(obj) === 'R') return false
   return figureOut(obj, workflows.find(({ name }) => name === wf.fn(obj)))
 }
 
-const accepted = objs.filter((obj) => figureOut(obj, intakeWorkflow))
-
-const out = accepted.reduce((acc, curr) => acc + Object.values(curr).reduce((sum, num) => sum + num), 0)
+const out = objs
+  .filter((obj) => figureOut(obj))
+  .reduce((acc, curr) => acc + Object.values(curr).reduce((sum, num) => sum + num), 0)
 
 console.log(out)
