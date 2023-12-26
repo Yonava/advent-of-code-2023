c4c06b9ccb90db7933ac7dbb874e072f3a5e5788
yonava <yona@vossandreae.com>
2023-12-26 00:11:24 -0700

    day 16 polish

--- day16.js
+++ day16.js
@@ -18,8 +18,8 @@ const splitters = {
 const visited = new Set()
 const tilesHit = new Set()
 
-const dfs = async (x, y, dir) => {
-  await new Promise(res => setTimeout(res, 10))
+const dfs = async (y, x, dir) => {
+  // await new Promise(res => setTimeout(res, 500))
   const tile = maze?.[y]?.[x]
   if (tile === undefined || visited.has(`${y},${x},${dir}`)) return
   visited.add(`${y},${x},${dir}`)
@@ -28,10 +28,10 @@ const dfs = async (x, y, dir) => {
   mazeCpy[y][x] = '#'
   console.log(mazeCpy.map(l => l.join('')))
 
-  const go = async (newDir) => await dfs(x + dirs[newDir][1], y + dirs[newDir][0], newDir)
+  const go = async (newDir) => await dfs(y + dirs[newDir][0], x + dirs[newDir][1], newDir)
 
   if (reflectors[tile]) return await go(reflectors[tile][dir])
-  if (splitters[tile]?.[dir]) return await Promise.all(splitters[tile][dir].map((d) => go(d)))
+  if (splitters[tile]?.[dir]) return await Promise.all(splitters[tile][dir].map(go))
 
   await go(dir)
 }
@@ -41,21 +41,21 @@ const dfs = async (x, y, dir) => {
 
 // PART 2
 const pt2 = async () => {
-  const trials = []
+  let maxEnergized = 0
   const run = async (...args) => {
     await dfs(...args)
-    trials.push(tilesHit.size)
+    maxEnergized = Math.max(tilesHit.size, maxEnergized)
     mazeCpy = [...maze].map(l => l.split(''))
     tilesHit.clear()
     visited.clear()
   }
 
-  for (const i in maze) await run(0, i, 'e')
-  for (const i in maze) await run(maze[0].length - 1, i, 'w')
-  for (let i = 0; i < maze[0].length; i++) await run(i, 0, 's')
-  for (let i = 0; i < maze[0].length; i++) await run(i, maze[0].length - 1, 'n')
+  for (const i in maze) await run(i, 0, 'e')
+  for (const i in maze) await run(i, maze[0].length - 1, 'w')
+  for (let i = 0; i < maze[0].length; i++) await run(0, i, 's')
+  for (let i = 0; i < maze[0].length; i++) await run(maze[0].length - 1, i, 'n')
 
-  return Math.max(...trials)
+  return maxEnergized
 }
 
 pt2().then(console.log)
