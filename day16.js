43cbb57bdda15d0649acd20b436a803e67635ca0
yonava <yona@vossandreae.com>
2023-12-25 23:49:11 -0700

    day 16 w/visuals

--- day16.js
+++ day16.js
@@ -1,76 +1,61 @@
 const { readFileSync } = require('fs')
 
 const maze = readFileSync('./input.txt').toString().split('\n')
+let mazeCpy = [...maze].map(l => l.split(''))
 
-const dirs = {
-  n: [-1, 0],
-  e: [0, 1],
-  w: [0, -1],
-  s: [1, 0]
+const dirs = { n: [-1, 0], e: [0, 1], w: [0, -1], s: [1, 0] }
+
+const reflectors = {
+  '\\': { e: 's', s: 'e', w: 'n', n: 'w' },
+  '/': { e: 'n', s: 'w', w: 's', n: 'e' }
+}
+
+const splitters = {
+  '|': { e: ['n', 's'], w: ['n', 's'] },
+  '-': { n: ['e', 'w'], s: ['e', 'w'] }
 }
 
 const visited = new Set()
 const tilesHit = new Set()
 
 const dfs = async (x, y, dir) => {
+  await new Promise(res => setTimeout(res, 10))
   const tile = maze?.[y]?.[x]
   if (tile === undefined || visited.has(`${y},${x},${dir}`)) return
   visited.add(`${y},${x},${dir}`)
   tilesHit.add(`${y},${x}`)
-  switch (tile) {
-    case '\\':
-      if (dir === 'e') return dfs(x + dirs.s[1], y + dirs.s[0], 's')
-      else if (dir === 'w') return dfs(x + dirs.n[1], y + dirs.n[0], 'n')
-      else if (dir === 'n') return dfs(x + dirs.w[1], y + dirs.w[0], 'w')
-      else return dfs(x + dirs.e[1], y + dirs.e[0], 'e')
-    case '/':
-      if (dir === 'e') return dfs(x + dirs.n[1], y + dirs.n[0], 'n')
-      else if (dir === 'w') return dfs(x + dirs.s[1], y + dirs.s[0], 's')
-      else if (dir === 'n') return dfs(x + dirs.e[1], y + dirs.e[0], 'e')
-      else return dfs(x + dirs.w[1], y + dirs.w[0], 'w')
-    case '|':
-      if (dir === 'n' || dir === 's') break
-      dfs(x + dirs.n[1], y + dirs.n[0], 'n')
-      dfs(x + dirs.s[1], y + dirs.s[0], 's')
-      return
-    case '-':
-      if (dir === 'e' || dir === 'w') break
-      dfs(x + dirs.e[1], y + dirs.e[0], 'e')
-      dfs(x + dirs.w[1], y + dirs.w[0], 'w')
-      return
-  }
-  dfs(x + dirs[dir][1], y + dirs[dir][0], dir)
+
+  mazeCpy[y][x] = '#'
+  console.log(mazeCpy.map(l => l.join('')))
+
+  const go = async (newDir) => await dfs(x + dirs[newDir][1], y + dirs[newDir][0], newDir)
+
+  if (reflectors[tile]) return await go(reflectors[tile][dir])
+  if (splitters[tile]?.[dir]) return await Promise.all(splitters[tile][dir].map((d) => go(d)))
+
+  await go(dir)
 }
+
+// PART 1
+// dfs(0, 0, 'e').then(() => console.log('done'))
 
 // PART 2
+const pt2 = async () => {
+  const trials = []
+  const run = async (...args) => {
+    await dfs(...args)
+    trials.push(tilesHit.size)
+    mazeCpy = [...maze].map(l => l.split(''))
+    tilesHit.clear()
+    visited.clear()
+  }
 
-const trials = []
-for (let i = 0; i < maze.length; i++) {
-  dfs(0, i, 'e')
-  trials.push(tilesHit.size)
-  tilesHit.clear()
-  visited.clear()
+  for (const i in maze) await run(0, i, 'e')
+  for (const i in maze) await run(maze[0].length - 1, i, 'w')
+  for (let i = 0; i < maze[0].length; i++) await run(i, 0, 's')
+  for (let i = 0; i < maze[0].length; i++) await run(i, maze[0].length - 1, 'n')
+
+  return Math.max(...trials)
 }
 
-for (let i = 0; i < maze.length; i++) {
-  dfs(maze[0].length - 1, i, 'w')
-  trials.push(tilesHit.size)
-  tilesHit.clear()
-  visited.clear()
-}
-
-for (let i = 0; i < maze[0].length; i++) {
-  dfs(i, 0, 's')
-  trials.push(tilesHit.size)
-  tilesHit.clear()
-  visited.clear()
-}
-
-for (let i = 0; i < maze[0].length; i++) {
-  dfs(i, maze[0].length - 1, 'n')
-  trials.push(tilesHit.size)
-  tilesHit.clear()
-  visited.clear()
-}
-
-console.log('done', Math.max(...trials))
+pt2().then(console.log)
