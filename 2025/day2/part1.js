6f166db10eafbfaba38056d8e06b2625fb7103b1
yonava <yonavossandreae@gmail.com>
2025-12-02 22:07:46 -0800

    solved

--- 2025/day2/part1.js
+++ 2025/day2/part1.js
@@ -1,14 +1,19 @@
 import { input } from "./input.js";
 
-const badIds = [];
-for (const line of input.split(',')) {
-  const [bottom, top] = line.split('-')
-  for (let i = Number(bottom); i <= Number(top); i++) {
-    const str = i.toString();
-    const first = str.slice(0, Math.floor(str.length / 2))
-    const second = str.slice(Math.floor(str.length / 2))
-    if (first === second) badIds.push(Number(str))
-  }
-}
+const test = (str) => Array(Math.floor(str.length / 2))
+  .fill(0)
+  .map((_, i) => str.slice(0, i + 1))
+  .map((s) => str.split(s).join(''))
+  .some((s) => !s)
 
-console.log(badIds.reduce((acc, curr) => acc + curr))
+const output = input
+  .split(',')
+  .map((line) => {
+    const [bottom, top] = line.split('-')
+    return Array(Number(top) - Number(bottom) + 1)
+      .fill(0)
+      .map((_, i) => Number(bottom) + i)
+      .filter((n) => test(n.toString()));
+  })
+  .flat()
+  .reduce((acc, curr) => acc + curr);
